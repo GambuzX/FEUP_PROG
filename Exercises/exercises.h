6b032ec2c16abd21066dd63929004a2e81890cc8
Mário Gil <mg_mesquita@hotmail.com>
2018-03-02 15:57:22 +0000

    Created a header file with all the exercises definitions

---
+++ Exercises/exercises.h
@@ -0,0 +1,41 @@
+#pragma once
+
+class Exercises
+{
+public:
+	void intro_exe();
+	void exe1_1();
+	void exe1_2();
+	void exe1_3();
+	void exe1_4();
+	void exe1_5();
+	float exe1_6();
+	void exe2_2_a();
+	void exe2_2_b();
+	void exe2_2_c();
+	void exe2_3();
+	void exe2_4();
+	void exe2_5();
+	void exe2_6_a();
+	bool isPrime(int number);
+	void exe2_6_b();
+	void exe2_6_c();
+	void exe2_7_a();
+	void exe2_7_b();
+	void exe2_9_a();
+	void exe2_9_b();
+	void exe2_9_c();
+	void exe2_10_a();
+	void exe2_10_b();
+	double exe2_11_a();
+	double exe2_11_b();
+	double exe2_11_c();
+	void exe2_13();
+	void exe2_14_a();
+	void exe2_14_b();
+	int exe3_8(int m, int n);
+
+private:
+
+
+};
