1947407faaacd38d84f4dc9563e262bc5acf8259
Mário Gil <mg_mesquita@hotmail.com>
2018-04-15 10:57:39 +0100

    Defined all the Date class methods

--- Exercises/OOP Classes.cpp
+++ Exercises/OOP Classes.cpp
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                    DATE                                                          //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 Date::Date(unsigned int year, unsigned int month, unsigned int day)
 {
 	this->day = day;
@@ -18,6 +22,55 @@ Date::Date(std::string yearMonthDay)
 	istringstream istr(yearMonthDay);
 	char bar;
 	istr >> year >> bar >> month >> bar >> day;
+}
+
+void Date::setYear(unsigned int year)
+{
+	this->year = year;
+}
+
+void Date::setMonth(unsigned int month)
+{
+	this->month = month;
+}
+
+void Date::setDay(unsigned int day)
+{
+	this->day = day;
+}
+
+void Date::setDate(unsigned int year, unsigned int month, unsigned int day)
+{
+	this->day = day;
+	this->month = month;
+	this->year = year;
+}
+
+unsigned int Date::getYear()
+{
+	return year;
+}
+
+unsigned int Date::getMonth()
+{
+	return month;
+}
+
+unsigned int Date::getDay()
+{
+	return day;
+}
+
+std::string Date::getDate()
+{
+	ostringstream outs;
+	outs << year << '/' << month << '/' << day;
+	return outs.str();
+}
+
+void Date::show()
+{
+	cout << getDate() << endl;
 }
 
 
