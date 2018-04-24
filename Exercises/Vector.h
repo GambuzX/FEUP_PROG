d91136111e1011802e3bcfc83cb985892630d303
Mário Gil <mg_mesquita@hotmail.com>
2018-04-24 10:26:29 +0100

    Finished Vector class

--- Exercises/Vector.h
+++ Exercises/Vector.h
@@ -1,6 +1,8 @@
 #ifndef VECTOR
 #define VECTOR
 
+#include <stdlib.h>
+
 template <typename T>
 class Vector
 {
@@ -50,8 +52,78 @@ template <typename T>
 Vector::Vector(const Vector<T> & v)
 {
 	buffer = new T[v.size()];
-	for (int i = 0; i < v.size(); i++)
+	for (size_t i = 0; i < v.size(); i++)
 		*(buffer + i) = v.at(i);
 	bufferSize = v.size();
 }
+
+template <typename T>
+Vector::~Vector()
+{
+	delete [] buffer;
+}
+
+template <typename T>
+size_t Vector::size() const
+{
+	return bufferSize;
+}
+
+template <typename T>
+bool empty() const
+{
+	return buffer == NULL;
+}
+
+template <typename T>
+T & Vector::front()
+{
+	return *buffer;
+}
+
+template < typename T>
+T & Vector::back()
+{
+	return *(buffer + bufferSize - 1);
+}
+
+template <typename T>
+T & Vector::at(size_t index)
+{
+	return *(buffer + index);
+}
+
+template <typename T>
+void Vector::push_back(const T & value)
+{
+	T * newBuffer = realloc(buffer, bufferSize + 1);
+	bufferSize++;
+	*(newBuffer + bufferSize - 1) = value;
+	buffer = newBuffer;
+}
+
+template <typename T>
+void Vector::pop_back()
+{
+	if (bufferSize = 1)
+	{
+		bufferSize = 0;
+		buffer = NULL;
+	}
+	else
+	{
+		T * newBuffer = realloc(buffer, bufferSize - 1);
+		bufferSize--;
+		buffer = newBuffer;
+	}
+}
+
+template <typename T>
+void Vector::clear()
+{
+	delete[] buffer;
+	buffer = NULL;
+	bufferSize = 0;
+}
+
 #endif
